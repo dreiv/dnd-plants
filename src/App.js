f9074e382c7b95b843733241b8e39816b6586270
Andrei Voicu <dreivoicu@gmail.com>
2021-05-23 14:55:29 +0300

    link to google sheets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import './App.css';
+import React, { useEffect, useState, Fragment } from "react";
+import Tabletop from "tabletop";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    Tabletop.init({
+      key: "1joq6oGR_nb_mHa4-w2r-EC50qWim7mWvqiN_K1WxGRE",
+      simpleSheet: true,
+    })
+      .then((data) => setData(data))
+      .catch((err) => console.warn(err));
+  }, []);
+
   return (
-    <div className="App">
-      hello world!
-    </div>
+    <>
+      <h1>data from google sheets</h1>
+      <ul>
+        {data.map((item, i) => (
+          <Fragment key={i}>
+            <li>URL -- {item.URL}</li>
+            <li>Email - {item.email}</li>
+            <li>Token - {item.token}</li>
+            <br />
+          </Fragment>
+        ))}
+      </ul>
+    </>
   );
 }
 
