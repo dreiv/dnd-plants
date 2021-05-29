16cb81ee653e447c827ea1b0055a28552d809cbd
Andrei Voicu <dreivoicu@gmail.com>
2021-05-29 21:14:57 +0300

    roll

--- src/components/items/Items.js
+++ src/components/items/Items.js
@@ -9,6 +9,7 @@ export function Items() {
   const renderItem = (item) => (
     <li className="item">
       <h3>{item.name}</h3>
+      <p>Probability: {item.probability}%</p>
       <p className="detail-container">Season: {item.season.join(", ")}</p>
       <p className="detail-container">Climate: {item.climate.join(", ")}</p>
       <p className="detail-container">Locale: {item.locale.join(", ")}</p>
--- src/components/roll/Roll.js
+++ src/components/roll/Roll.js
@@ -1,10 +1,21 @@
 import React from "react";
 
+import { useAppState } from "app-context";
 import "./Roll.css";
 
 export function Roll() {
+  const { filteredData } = useAppState();
+
   const handleClick = () => {
-    alert("Not implemented!");
+    const loot = [...filteredData].reduce((acc, item) => {
+      const rand = Math.floor(Math.random() * 100 + 1);
+
+      if (rand <= +item.probability) acc.push(item.name);
+
+      return acc;
+    }, []);
+
+    alert(`loot: ${loot.toString()}`);
   };
 
   return (
